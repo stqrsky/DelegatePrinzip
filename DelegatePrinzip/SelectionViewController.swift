01768970823bbce6aa0e669380d31a08e002c85f
Starsky <starsky.m@live.com>
2022-01-21 14:24:40 +0100

    UI first

---
+++ DelegatePrinzip/SelectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SelectionViewController.swift
+//  DelegatePrinzip
+//
+//  Created by star on 21.01.22.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
